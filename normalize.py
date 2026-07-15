e976b0127a703494ab008003804e08183cf7c705
marco <manciomarco04@gmail.com>
2026-07-15 21:00:34 +0200

    spostato normalize.py nella cartella principale

---
+++ normalize.py
@@ -0,0 +1,28 @@
+import ast
+import pandas as pd
+from config import FULL_DATASET, FORMATTED_DATASET
+
+dataset = pd.read_csv(FULL_DATASET)
+
+# filtro le colonne prendendo solo quelle informative
+dataset = dataset[["title", "ingredients", "directions", "NER"]]
+
+dataset = dataset.drop_duplicates()
+dataset = dataset.dropna()
+
+# normalizzo il testo e converto da str a list
+dataset["title"] = dataset["title"].str.strip().str.lower()
+dataset["ingredients"] = dataset["ingredients"].str.strip().str.lower().apply(ast.literal_eval)
+dataset["directions"] = dataset["directions"].str.strip().str.lower().apply(ast.literal_eval)
+dataset["NER"] = dataset["NER"].str.strip().str.lower().apply(ast.literal_eval)
+
+# aggiungo una colonna che contiene il numero di ingredienti
+dataset["#ingredients"] = dataset["NER"].apply(len)
+
+# aggiungo una colonna che contiene il numero di step
+dataset["#steps"] = dataset["directions"].apply(len)
+
+# aggiungo una colonna che contiene il numero di parole delle istruzioni
+dataset["directions_len"] = dataset["directions"].astype(str).apply(lambda x: len(x.split()))
+
+dataset.to_parquet(FORMATTED_DATASET)
